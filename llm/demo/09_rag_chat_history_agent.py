ed6ccf305e9bbd742f3787eea40aa9cc978e8cf0
CcccFz <ccccfz@163.com>
2025-02-24 23:37:41 +0800

    feat: llm sql chat qa

--- llm/demo/09_rag_chat_history_agent.py
+++ llm/demo/09_rag_chat_history_agent.py
@@ -18,16 +18,12 @@ import bs4
 model = ChatOpenAI(model="gpt-4o-mini", base_url="https://www.dmxapi.com/v1")
 parser = StrOutputParser()
 
-from datetime import datetime
-print(1, datetime.now())
-
 loader = WebBaseLoader(
     web_paths=['https://lilianweng.github.io/posts/2023-06-23-agent'],
     bs_kwargs={'parse_only': bs4.SoupStrainer(
         class_=('post-header', 'post-title', 'post-content'))}
 )
 docs = loader.load()
-print(2, datetime.now())
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=200, add_start_index=True)
 splites = splitter.split_documents(docs)
@@ -35,13 +31,11 @@ vector_store = Chroma.from_documents(
     documents=splites,
     embedding=OpenAIEmbeddings(base_url='https://www.dmxapi.com/v1'),
 )
-print(3, datetime.now())
 retriever = vector_store.as_retriever(
     search_type="similarity",
     search_kwargs={"k": 6}
 )
 
-print(4, datetime.now())
 ### Build retriever tool ###
 tool = create_retriever_tool(
     retriever,
@@ -52,8 +46,6 @@ tools = [tool]
 
 memory = MemorySaver()
 agent_executor = create_react_agent(model, tools, checkpointer=memory)
-print(5, datetime.now())
-
 
 for s in agent_executor.stream(
     {"messages": [HumanMessage(content="Hi! I'm bob")]},
@@ -61,14 +53,14 @@ for s in agent_executor.stream(
 ):
     print(s)
     print("----")
-print(6, datetime.now())
+
 for s in agent_executor.stream(
     {"messages": [HumanMessage(content="What is Task Decomposition?")]},
     config={"configurable": {"thread_id": "abc123"}},
 ):
     print(s)
     print("----")
-print(7, datetime.now())
+
 for s in agent_executor.stream(
     {"messages": [HumanMessage(content="What according to the blog post are common ways of doing it? redo the search")]},
     # {"messages": [HumanMessage(content="What are common ways of doing it?")]},
@@ -76,4 +68,3 @@ for s in agent_executor.stream(
 ):
     print(s)
     print("----")
-print(8, datetime.now())
